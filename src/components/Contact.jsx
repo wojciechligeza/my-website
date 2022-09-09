04f5e253cf2c19b9f2f3bdb0e9a2b9226ea833b6
wojLi <wojciech.ligeza@icloud.com>
2022-09-09 11:05:51 +0200

    feat: download CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,5 @@
+import cv from '../assets/wl_cv_en.pdf'
+
 export default function Contact() {
   return (
     <nav className="m-4 flex items-center justify-center gap-8 sm:hidden">
@@ -23,7 +25,7 @@ export default function Contact() {
         </a>
       </div>
       <div role="button" className="p-2 transition-transform duration-300 hover:scale-110">
-        <a href="https://www.linkedin.com/in/wojciech-lig%C4%99za-9883171a1/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/wojciech-lig%C4%99za-9883171a1/" target="_blank" rel="noreferrer noopener">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 310 310"
@@ -46,6 +48,27 @@ export default function Contact() {
 		C310,145.43,300.549,94.761,230.454,94.761z"
               />
             </g>
+          </svg>
+        </a>
+      </div>
+      <div
+        role="button"
+        className="rounded-xl bg-white p-2 shadow transition duration-300 hover:scale-105 hover:shadow-lg"
+      >
+        <a href={cv} download="Wojciech's CV">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-8 w-8 text-slate-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25"
+            />
           </svg>
         </a>
       </div>
